10d051c527666f69cba048e2649a162972d1eaad
Norman Herrera <norman.herrera@ymail.com>
2023-01-14 12:10:13 -0800

    api call works on component did mount and jokes are rendered

--- src/Machine.js
+++ src/Machine.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import './Machine.css';
 import Joke from './Joke.js';
+import axios from 'axios';
 
 class Machine extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { jokes: []}
+    }
+    static defaultProps = {
+        numJokes : 10
+    };
+    async componentDidMount() {
+        let jokes = [];
+        while(jokes.length < this.props.numJokes){
+            let res = await axios.get("https://icanhazdadjoke.com/", {
+                headers: { Accept: "application/json"}
+            });
+            jokes.push(res.data.joke)
+        }
+        this.setState({jokes: jokes});
+    }
+
+
     render() {
         return (
-            <div>
-                <Joke />
+            <div className='Machine'>
+                <h1>Dad Jokes</h1>
+                <div className='Machine-Jokes'>
+                    {this.state.jokes.map(j => (
+                        <div>{j}</div>
+                    ))}
+                </div>
             </div>
         )
     }
