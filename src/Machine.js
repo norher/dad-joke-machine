48f827aa29668dfde239442aec7260065a987135
Norman Herrera <norman.herrera@ymail.com>
2023-01-14 11:22:03 -0800

    main app container

---
+++ src/Machine.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Machine extends Component {
+    render() {
+        return (
+            <h1>Machine goes here!</h1>
+        )
+    }
+}
+
+export default Machine;
