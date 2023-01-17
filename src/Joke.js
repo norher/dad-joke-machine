d984e25bc5c62fe8b1dc300a7638fae73a5e0982
Norman Herrera <norman.herrera@ymail.com>
2023-01-17 15:50:40 -0800

    styled joke component, added some icons and emojis

--- src/Joke.js
+++ src/Joke.js
@@ -7,11 +7,14 @@ class Joke extends Component {
             <div className='Joke'>
                 <div className='Joke-buttons'>
                     <i className='fas fa-arrow-up'onClick={this.props.upvote}/>
-                    <span>{this.props.votes}</span>
+                    <span className='Joke-votes'>{this.props.votes}</span>
                     <i className='fas fa-arrow-down'onClick={this.props.downvote}/>
                 </div>
                 <div className='Joke-text'>
                     {this.props.text}
+                </div>
+                <div className='Joke-smiley'>
+                    <i class="em em-rolling_on_the_floor_laughing" aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
                 </div>
             </div>
         )
