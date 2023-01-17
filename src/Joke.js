dda8a68bf7cca5c4955c629568eae12ff221be0a
Norman Herrera <norman.herrera@ymail.com>
2023-01-17 15:34:52 -0800

    more styling. Joke components developed and passed on. voting mechanism works

--- src/Joke.js
+++ src/Joke.js
@@ -4,11 +4,15 @@ import './Joke.css';
 class Joke extends Component {
     render() {
         return (
-            <div>
-                <button>Upvote</button>
-                <text>Counter</text>
-                <button>Downvote</button>
-                <text>Joke here rendered by React</text>
+            <div className='Joke'>
+                <div className='Joke-buttons'>
+                    <i className='fas fa-arrow-up'onClick={this.props.upvote}/>
+                    <span>{this.props.votes}</span>
+                    <i className='fas fa-arrow-down'onClick={this.props.downvote}/>
+                </div>
+                <div className='Joke-text'>
+                    {this.props.text}
+                </div>
             </div>
         )
     }
--- src/Machine.js
+++ src/Machine.js
@@ -17,9 +17,17 @@ class Machine extends Component {
             let res = await axios.get("https://icanhazdadjoke.com/", {
                 headers: { Accept: "application/json"}
             });
-            jokes.push(res.data.joke)
+            jokes.push({ text: res.data.joke, id: res.data.id, votes: 0 });
         }
         this.setState({jokes: jokes});
+    }
+    handleVote(id, delta) {
+        this.setState(
+            st => ({
+                jokes: st.jokes.map(j =>
+                    j.id === id ? { ...j, votes: j.votes + delta } : j )
+            })
+        )
     }
 
 
@@ -36,11 +44,16 @@ class Machine extends Component {
                         height={325}
                         width={325} 
                     />
-                    <button>Get Jokes!</button>
+                    <button className='Machine-getmore'>Get Jokes!</button>
                 </div>
                 <div className='Machine-Jokes'>
                     {this.state.jokes.map(j => (
-                        <div>{j}</div>
+                        <Joke 
+                            key={j.id} 
+                            votes={j.votes} 
+                            text={j.text} 
+                            upvote={() => this.handleVote(j.id, 1)}
+                            downvote={() => this.handleVote(j.id, -1)} />
                     ))}
                 </div>
             </div>
